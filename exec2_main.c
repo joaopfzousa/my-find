fb2f0384498b00b11c5ce8b00004740c1556ebf0
João Sousa <33814@ufp.edu.pt>
2020-05-23 23:44:46 +0100

    added threads

--- exec2_main.c
+++ exec2_main.c
@@ -8,9 +8,15 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
 
 #define MAX_CMD 100
 #define TRUE 1
+
+void * listDir(void * param);
+void remove_all_chars(char* str, char c);
+void prompt();
 
 typedef int (*PARAM)(struct dirent * entry, char * value);
 
@@ -20,7 +26,7 @@ typedef struct arg {
 }ARG;
 
 typedef struct thread_data {
-	//path
+	char * base_path;
 	//...
     ARG args[50];
     int n_args;
@@ -83,83 +89,6 @@ int size (struct dirent * entry, char * value) {
     return 1; // return 1 if match found
 }
 
-
-void prompt(){
-    char path[100];
-    getcwd(path, sizeof(path));
-
-    char *res;
-    res = strrchr(path, '/');
-    printf("%s$", res);
-}
-
-void listDir(const char *name)
-{
-    DIR *dir;
-    struct dirent *entry;
-
-    struct stat file_stat;
-
-    char *base_path = malloc(sizeof(char) * 300);
-    char *path = malloc(sizeof(char) * 300);
-    strcpy(base_path, name);
-
-    if ((dir = opendir(base_path)) == NULL)
-        perror("opendir() error");
-    else
-    {
-        printf("\n\n%s\n", base_path);
-        while ((entry = readdir(dir)) != NULL)
-        {
-            sprintf(path, "%s%s", base_path, entry->d_name);
-            
-            if (stat(path, &file_stat) == 0)
-            {
-                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".DS_Store") == 0 || strcmp(entry->d_name, ".git") == 0)
-                    continue;
-
-                for (int i=0 ; i<t_data.n_args ; i++)
-                    if (!t_data.args[i].opt(path, t_data.args[i].value))
-                        break;
-
-                i == t_data.n_args ? printf("match\n") : printf("No match\n"); 
-
-                printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
-                
-                printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
-                printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
-                printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
-                printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
-                printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
-                printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
-                printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
-                printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
-                printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
-
-                printf(" %u\t%lld\t%s\n", file_stat.st_uid, file_stat.st_size, entry->d_name);
-
-                if(S_ISDIR(file_stat.st_mode))
-                {
-                    sprintf(path, "%s/", path);
-                    listDir(path);
-                }
-            } 
-            strcpy(path, "");
-        }
-        closedir(dir);
-    }
-}
-
-void remove_all_chars(char* str, char c) {
-    char *pr = str, *pw = str;
-    while (*pr) {
-        *pw = *pr++;
-        pw += (*pw != c);
-    }
-    *pw = '\0';
-}
-
-
 T_DATA read_command( char *cmd, char **arg_list)
 {
     char *token;
@@ -168,15 +97,16 @@ T_DATA read_command( char *cmd, char **arg_list)
     cmd[strlen(cmd)-1] = '\0';
     token = strtok(cmd, " ");
 
+    T_DATA t_data = { .args={NULL, ""}, .n_args=0 };
     if(strcmp(token, "find") == 0 )
     {
         arg_list[0] = token;
         token = strtok(NULL, " ");
-        arg_list[1] = token;
+        t_data.base_path = malloc(sizeof(char) * 300);
+        t_data.base_path = token;
         token = strtok(NULL, " ");
     }
-
-    T_DATA t_data = { .args={NULL, ""}, .n_args=0 };
+    
     while (token != NULL)
     {    
         if(strcmp(token, "-name") == 0 || strcmp(token, "-iname") == 0 || strcmp(token, "-type") == 0 || strcmp(token, "-empty") == 0|| strcmp(token, "-executable") == 0 || strcmp(token, "-mmin") == 0 || strcmp(token, "-size") == 0)
@@ -207,34 +137,107 @@ int main(int argc, char **argv)
     char cmd[MAX_CMD]; 
     char *arg_list[5];
 
-    
     while (TRUE)
     {
         prompt();
-        T_DATA t_data = { .args={NULL, ""}, .n_args=0 };
-        t_data = read_command (cmd, arg_list);
-
+        T_DATA thread_data;
+        pthread_t thread_id;
+        thread_data = read_command (cmd, arg_list);
         int i=0;
+
         if(strcmp(arg_list[0], "find") == 0)
         {            
-            char *caminho = arg_list[1];
-       
-            listDir(caminho);
-            // Loop over entries
-
-            
-            // for each entry
-            
-           
-            
-
-        // end loop over entries
-            //listDir("/Users/joaopfzousa/Documents/Faculdade/SO/", 0);
+            pthread_create(&thread_id, NULL, &listDir, &thread_data);
+            pthread_join(thread_id, NULL);
+                
         }else if(strcmp(arg_list[0], "clear") == 0)
         {
            printf("\033[H\033[J");
         }
     }
-
     return 0;
+}
+
+void * listDir(void * param)
+{
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) param;
+
+    T_DATA thread_data[20];
+    pthread_t thread_id[20];
+    int i = 0;
+
+    DIR *dir;
+    struct dirent *entry;
+    char *path = malloc(sizeof(char) * 300);
+    struct stat file_stat;
+
+    //printf("\n%s\n\n", my_data->base_path);
+
+    if ((dir = opendir(my_data->base_path)) == NULL)
+        perror("opendir() error");
+    else
+    {
+        while ((entry = readdir(dir)) != NULL)
+        {
+
+        sprintf(path, "%s%s", my_data->base_path, entry->d_name);
+
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".DS_Store") == 0 || strcmp(entry->d_name, ".git") == 0)
+            continue;
+
+        if (stat(path, &file_stat) == 0)
+        {
+
+            printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
+            
+            printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
+            printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
+            printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
+            printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
+            printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
+            printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
+            printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
+            printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
+            printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
+
+            printf(" %u\t%lld\t%s\n", file_stat.st_uid, file_stat.st_size, entry->d_name);
+
+            if(S_ISDIR(file_stat.st_mode))
+            {
+                path = strcat(path, "/");
+
+                thread_data[i].base_path = malloc(sizeof(char) * 300);
+                strcpy(thread_data[i].base_path, path);
+                pthread_create(&thread_id[i], NULL, &listDir, &thread_data[i]);
+                i++ ;
+            }
+        }
+
+        strcpy(path, "");
+        }
+        closedir(dir);
+    }
+
+    for(int j = 0; j < i; j++)
+        pthread_join(thread_id[j], NULL);
+    pthread_exit(NULL);
+}
+
+void remove_all_chars(char* str, char c) {
+    char *pr = str, *pw = str;
+    while (*pr) {
+        *pw = *pr++;
+        pw += (*pw != c);
+    }
+    *pw = '\0';
+}
+
+void prompt(){
+    char path[100];
+    getcwd(path, sizeof(path));
+
+    char *res;
+    res = strrchr(path, '/');
+    printf("%s$", res);
 }
